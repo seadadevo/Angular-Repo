29001043199b13631563fb1f54ac3ca9229806b9
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-10-29 15:14:44 +0300

    first commit for dashboard structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { ICourse } from './app.component.model';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProductsComponent } from './products/products.component';
 import { SettingsComponent } from './settings/settings.component';
@@ -17,8 +14,6 @@ import { MainComponent } from './main/main.component';
     LeftSidebarComponent,
     MainComponent,
     RouterOutlet,
-    HomeComponent,
-    CourseCardComponent,
     DashboardComponent,
     ProductsComponent,
     SettingsComponent,
@@ -28,54 +23,5 @@ import { MainComponent } from './main/main.component';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  data = {
-    title: 'angular-complete-guide',
-  };
 
-  onLogoClicked(): void {
-    alert('Hello world');
-  }
-
-  onkeyUp(title: string): void {
-    this.data.title = title;
-  }
-
-  readonly courses: Array<ICourse> = [
-    {
-      id: 1,
-      description: 'Angular 17 for beginners',
-      imageUrl: 'assets/images/front.jpg',
-      lessonsCount: 70,
-      longDescription: '',
-    },
-    {
-      id: 2,
-      description: 'RxJs',
-      imageUrl: 'assets/images/front.jpg',
-      lessonsCount: 40,
-      longDescription:
-        'We will then write our own Observable from first principles:',
-    },
-    {
-      id: 3,
-      description: 'NgRx',
-      imageUrl: 'assets/images/front.jpg',
-      lessonsCount: 48,
-      longDescription:
-        'In this course, we are going to take a small existing applica',
-    },
-  ];
-
-  oncCourseEnrolled(course: ICourse): void {
-    console.log('التليفزيون: وصلتني إشارة!');
-    alert(`تم الاشتراك في: ${course.description}`);
-  }
-
-  handleLogout(): void {
-    console.log('logged out');
-  }
-
-  trackCourse(index: number, course: ICourse): number {
-    return course.id;
-  }
 }
