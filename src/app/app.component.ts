8eccac58ef3cb6b0183b73d9ffd9c5e593f68d29
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-10-29 15:10:27 +0300

    create branch for the angular course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,26 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { ICourse } from './app.component.model';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ProductsComponent } from './products/products.component';
-import { SettingsComponent } from './settings/settings.component';
-import { PagesComponent } from './pages/pages.component';
-import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
-import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    LeftSidebarComponent,
-    MainComponent,
     RouterOutlet,
     HomeComponent,
     CourseCardComponent,
-    DashboardComponent,
-    ProductsComponent,
-    SettingsComponent,
-    PagesComponent,
+  
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
