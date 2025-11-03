c5ba8c6db84ae86fca6b5a0291163f2becf35a4a
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-11-03 19:15:06 +0200

    implement and connect Login form with backend API

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -14,5 +14,9 @@ export class AuthService {
   signup(userData:object):Observable<any>{
     return this._HttpClient.post('http://localhost:5000/api/users/signup', userData)
   }
+  
+  signin(userData:object):Observable<any>{
+    return this._HttpClient.post('http://localhost:5000/api/users/signin', userData)
+  }
  
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +11,51 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    constructor(private _AuthService:AuthService, private _Router: Router) {
+    }
+  
+    error: string = '';
+    isLoading: boolean = false;
+    showPassword: boolean = false;
+  
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  
+    loginForm:FormGroup = new FormGroup({
+      email:new FormControl(null, [Validators.email, Validators.required]),
+      password:new FormControl(null, [Validators.required, Validators.pattern(/^[A-Z]/)]),
+    })
+  
+  
+  
+  
+  
+  
+    submitLoginForm(loginForm:FormGroup) {
+      this.isLoading = true;
+      this._AuthService.signin(loginForm.value).subscribe({
+        next:(response)=>{
+          this.isLoading = false;
+          if(response.message === 'Login successful') {
+            this._Router.navigate(['/home'])
+            this.loginForm.reset();
+          } 
+        },
+        error:(err) => {
+          if(err.error&&err.error.message) {
+            this.error=err.error.message
+          } else {
+            this.error = 'Login failed. Please try again.';
+          }
+          console.error(err);
+        },
+      }).add(() => {
+      this.isLoading= false
+    })
+    }
+
+
+   
 
 }
