08a179ad7e7ef19e7ffcc81b1f7909044a4e03bc
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-11-09 03:20:08 +0200

    change watchList from depending on LocalStorage to DataBase

--- src/app/mediaitem/mediaitem.component.ts
+++ src/app/mediaitem/mediaitem.component.ts
@@ -1,3 +1,4 @@
+// ملف: mediaitem.component.ts
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
@@ -16,20 +17,37 @@ export class MediaitemComponent implements OnInit{
   @Input() item:any = {} 
 
   isInWatchlist: boolean = false;
-  constructor(private _WatchListService: WatchlistService){
-
-  }
+  
+  
+  private itemID_to_check: string | null = null; 
+  
+  constructor(private _WatchListService: WatchlistService){ }
 
   ngOnInit(): void {
-    this._WatchListService.isItemInWatchlist(this.item.id).subscribe(result => {
-      this.isInWatchlist = result;
-    })
+    
+    if (this.item.id) {
+      
+      this.itemID_to_check = this.item.id.toString();
+    } 
+    else if (this.item.movieID) { 
+     
+      this.itemID_to_check = this.item.movieID.toString();
+    }
+
+   
+    if (this.itemID_to_check) {
+      this._WatchListService.isItemInWatchlist(this.itemID_to_check).subscribe(result => {
+        this.isInWatchlist = result;
+      });
+    }
   }
   
   addItem(item:any){
     this._WatchListService.addToWatchList(item);
   }
+  
   removeItem(item:any){
+   
     this._WatchListService.removeFromWatchlist(item);
   }
 }
--- src/app/services/watchlist.service.ts
+++ src/app/services/watchlist.service.ts
@@ -1,47 +1,108 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WatchlistService {
+
+  private baseUrl = 'http://localhost:5000/api/favorites';
+
+
   private watchList = new BehaviorSubject<any[]>([]);
-  watchList$ = this.watchList.asObservable();
 
-  constructor() { 
-    const savedList = localStorage.getItem('watchList');
-    if(savedList) {
-      const parsedList = JSON.parse(savedList);
-      this.watchList.next(parsedList);
+  public watchList$ = this.watchList.asObservable();
+
+  constructor(
+    private _HttpClient: HttpClient,
+    private _AuthService: AuthService
+  ) {
+   
+    this._AuthService.userData.subscribe((user:any) => {
+      if (user) {
+       
+        this.loadFavoritesFromServer();
+      } else {
+      
+        this.watchList.next([]);
+      }
+    });
+  }
+
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('userToken');
+    return new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+  }
+
+
+  loadFavoritesFromServer() {
+    if (!localStorage.getItem('userToken')) {
+      return; 
     }
+
+    this._HttpClient.get<any>(`${this.baseUrl}/getFavorites`, { headers: this.getAuthHeaders() })
+      .subscribe(response => {
+       
+        if (response && response.Favorites) {
+          
+          this.watchList.next(response.Favorites);
+        }
+      });
   }
 
-  addToWatchList(item:any){
-    const currentList = this.watchList.getValue()
-    const itemExists = currentList.find(i => i.id === item.id )
-    if(!itemExists) {
-      const updatedList = [...currentList, item]
-      this.watchList.next(updatedList)
-      localStorage.setItem("watchList", JSON.stringify(updatedList))
-    } else {
-      console.log('item is aleady exist!', item)
+  addToWatchList(item: any) {
+    
+    const payload = {
+      movieName: item.title || item.name, 
+      imgUrl: 'https://image.tmdb.org/t/p/w500/' + item.poster_path, 
+      movieID: item.id.toString() 
+    };
 
+    
+    this._HttpClient.post<any>(`${this.baseUrl}/addToFavorites`, payload, { headers: this.getAuthHeaders() })
+      .pipe(
+        tap((response) => {
+          
+          const currentList = this.watchList.getValue();
+          this.watchList.next([...currentList, response.Favorite]);
+        })
+      )
+      .subscribe();
+  }
+
+  removeFromWatchlist(item: any) {
+    
+    const movie_id_to_delete = (item.id || item.movieID)?.toString();
+
+    if (!movie_id_to_delete) {
+      console.error("Item has no ID to delete:", item);
+      return;
     }
+
+   
+    this._HttpClient.delete(`${this.baseUrl}/removeFromFavorites/${movie_id_to_delete}`, { headers: this.getAuthHeaders() })
+      .pipe(
+        tap(() => {
+          const currentList = this.watchList.getValue();
+          const updatedList = currentList.filter(favItem => favItem.movieID !== movie_id_to_delete);
+          this.watchList.next(updatedList);
+        })
+      )
+      .subscribe();
   }
 
 
-  removeFromWatchlist(item:any) {
-    const currentList = this.watchList.getValue()
-    const updatedList = currentList.filter(i => i.id !== item.id)
-    this.watchList.next(updatedList)
-    localStorage.setItem("watchList", JSON.stringify(updatedList))
-  }
-
-
-  isItemInWatchlist(id: number): Observable<boolean> {
+  isItemInWatchlist(id: string): Observable<boolean> {
+    
     return this.watchList$.pipe(
       map(list => {
-        return !!list.find(i => i.id === id); 
+        return !!list.find(favItem => favItem.movieID === id);
       })
     );
   }
